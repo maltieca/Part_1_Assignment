3ed7d28ae53858698588cf7db5493c835527eced
Nik <38010634+maltieca@users.noreply.github.com>
2020-01-07 01:41:38 +0100

    Created all method guards

--- Model-Based Testing Code/ModelBasedTesting/src/test/java/com/uom/cps3230/ModelWebsiteSystemTest.java
+++ Model-Based Testing Code/ModelBasedTesting/src/test/java/com/uom/cps3230/ModelWebsiteSystemTest.java
@@ -2,7 +2,9 @@ package com.uom.cps3230;
 
 
 import com.uom.cps3230.enums.ModelWebsiteStates;
+import nz.ac.waikato.modeljunit.Action;
 import nz.ac.waikato.modeljunit.FsmModel;
+import org.junit.Assert;
 
 public class ModelWebsiteSystemTest implements FsmModel {
     private ModelWebsiteStates modelState;
@@ -26,5 +28,43 @@ public class ModelWebsiteSystemTest implements FsmModel {
             sut = new ModelWebsiteSystem();
         }
     }
+
+    public boolean logOutGuard() {
+        return getState().equals(ModelWebsiteStates.LOG_ON);
+    }
+
+    public boolean logInGuard() {
+        return getState().equals(ModelWebsiteStates.LOG_OFF);
+    }
+
+    public boolean searchGuard() {
+        return getState().equals(ModelWebsiteStates.LOG_ON);
+    }
+
+    public boolean selectProductGuard() {
+        return getState().equals(ModelWebsiteStates.SEARCH);
+    }
+
+    public boolean findProductsAfterAddingGuard() {
+        return getState().equals(ModelWebsiteStates.ADD_PRODUCT);
+    }
+
+    public boolean removeProductsGuard() {
+        return getState().equals(ModelWebsiteStates.ADD_PRODUCT);
+    }
+
+    public boolean findProductsAfterRemovingGuard() {
+        return getState().equals(ModelWebsiteStates.REMOVE_PRODUCT);
+    }
+
+    public boolean checkoutAfterRemoveGuard() {
+        return getState().equals(ModelWebsiteStates.REMOVE_PRODUCT);
+    }
+
+    public boolean checkoutAfterAddingAProductGuard() {
+        return getState().equals(ModelWebsiteStates.ADD_PRODUCT);
+    }
+
+
 }
 
