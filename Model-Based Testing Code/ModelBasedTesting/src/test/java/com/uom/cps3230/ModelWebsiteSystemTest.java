bdfa3f24ff3f668fc83ac82bd01604e1d1b6c79e
Nik <38010634+maltieca@users.noreply.github.com>
2020-01-07 01:35:27 +0100

    Created getState() and reset()

--- Model-Based Testing Code/ModelBasedTesting/src/test/java/com/uom/cps3230/ModelWebsiteSystemTest.java
+++ Model-Based Testing Code/ModelBasedTesting/src/test/java/com/uom/cps3230/ModelWebsiteSystemTest.java
@@ -1,6 +1,30 @@
 package com.uom.cps3230;
 
 
-public class ModelWebsiteSystemTest{
+import com.uom.cps3230.enums.ModelWebsiteStates;
+import nz.ac.waikato.modeljunit.FsmModel;
 
+public class ModelWebsiteSystemTest implements FsmModel {
+    private ModelWebsiteStates modelState;
+    private boolean logOn, logOff, search, addProduct, removeProduct, checkout;
+    private ModelWebsiteSystem sut;
+
+
+    public ModelWebsiteStates getState() {
+        return modelState;
+    }
+
+    public void reset(final boolean b) {
+        modelState = ModelWebsiteStates.LOG_ON;
+        logOn = true;
+        logOff = false;
+        search = false;
+        addProduct = false;
+        removeProduct = false;
+        checkout = false;
+        if (b) {
+            sut = new ModelWebsiteSystem();
+        }
+    }
 }
+
